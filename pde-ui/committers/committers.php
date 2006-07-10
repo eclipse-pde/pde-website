dc2a3792d90df5f4d1cd4aa189cc83ff523febbc
wmelhem <wmelhem>
2006-07-10 04:44:11 +0000

    added committer photos

--- pde-ui/committers/committers.php
+++ pde-ui/committers/committers.php
@@ -19,51 +19,30 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 	  <div class="homeitem3col">
 			<a name="jdt ui"></a>
-			<h3>PDE UI</h3>
-			<p>The PDE UI component provides a comprehensive set of tools to create, develop, test, debug and deploy Eclipse   plug-ins, fragments, features, update sites and RCP products. </p>
-			<p> PDE UI also provides comprehensive OSGi tooling, which makes it an ideal environment   for component programming, not just Eclipse plug-in development.</p>
-			<p>Some of the PDE UI tools include:</p>
-			<ul>
-				<li>Form-Based Manifest Editors - multi-page editors that centrally manage all manifest files of a plug-in or feature. </li>
-				<li>RCP Tools - wizards and a form-based editor that allow you to define, brand, test and export products to multiple platforms. </li>
-				<li>New Project Creation Wizards - create a new plug-in, fragment, feature, feature patch and update sites.</li>
-				<li>Import Wizards - import plug-ins and features from the file system.</li>
-				<li>Export Wizards  - wizards that build, package and export plug-ins, fragments and products with a single click. </li>
-				<li>Launchers - test and debug Eclipse applications and OSGi bundles. </li>
-				<li>Plug-ins View - shows a unified view of the plug-ins you are developing and the plug-ins you are developing against. </li>
-			    <li>Plug-in Registry View - shows the status  of all plug-ins in the running instance of Eclipse. </li>
-			    <li>Error Log View - shows all the errors and warnings logged by plug-ins. </li>
-			    <li>Miscelllaneous Tools   - wizards to externalize and clean up manifest files.</li>
-			    <li>Conversion Tools - wizard to convert a plain Java project or plain JARs into a plug-in project. </li>
-			    <li>Refactoring Participation - plug-in manifest files are updated when Java types and packages are renamed or moved.</li>
-			    <li>Search Participation - search hits are shown in plug-in manifest files when you  search for a Java type or package. </li>
-		    </ul>
-	        <p>&nbsp;</p>
-	  </div>
-		<div class="homeitem3col">
-			<a name="happening"></a>
-			<h3>What&#8217;s Happening?</h3>
-			<p><b>R3.3 Development</b></p>
-			<p>During the Eclipse 3.3 development cycle, focus will be on the following areas:</p>
-			<ul>
-			  <li>API Tools</li>
-		      <li>Bundle/Module Development Tools</li>
-		      <li>Improved Launching Support</li>
-		      <li>Target Provisioning</li>
-		      <li>Bundle Management</li>
-		      <li>User Assistance Tooling      </li>
-	      </ul>
-	        <p>Watch this space for more details when the 3.3 plan is posted. </p>
-		</div>
-	  <div class="homeitem3col">
-			<a name="involved"></a>
-			<h3>Get Involved!</h3>
-		  <p>If you are interested in participating in the development of the
-			PDE UI component, check out the developer's mailing list: <a
-				href="http://dev.eclipse.org/mailman/listinfo/pde-ui-dev">pde-ui-dev@eclipse.org</a>.
-			Chat with people there about your problems and interests, and find
-		  out what you can do to help.</p>
-	  </div>		
+			<h3>PDE/UI Committers and Contributors</h3>
+			<table width="61%" border="0">
+              <tr>
+                <td colspan="2"><div align="center"><strong>Active Committers </strong></div></td>
+              </tr>
+              <tr>
+                <td width="50%" height="435"><p align="center"><img src="brian_bauman.gif" alt="Brian Bauman" width="200" height="280"></p>
+                <p align="center"><strong>Brian Bauman </strong></p>
+                <p align="center">Committer, IBM Austin Labs </p></td>
+                <td width="50%" height="435"><p align="center"><img src="dejan_glozic.gif" alt="Dejan Glozic" width="200" height="280"></p>
+                  <p align="center"><strong>Dejan Glozic</strong></p>
+                  <p align="center">Father of PDE, IBM Toronto Lab</p>                </td>
+              </tr>
+              <tr>
+                <td><p align="center"><img src="janek_lasocki_biczysko.gif" alt="Janek Lasocki Biczysko" width="200" height="280"></p>
+                <p align="center"><strong>Janek Lasocki-Biczysko</strong></p>
+                <p align="center">Committer, IBM Toronto Lab </p></td>
+                <td><p align="center"><img src="wassim_melhem.gif" alt="Wassim Melhem" width="200" height="280"></p>
+                <p align="center"><strong>Wassim Melhem</strong></p>
+                <p align="center">Component Lead, IBM Toronto Lab  </p></td>
+              </tr>
+            </table>
+			<p>&nbsp;</p>
+</div>
 	</div>
 <?php
 	$html = ob_get_contents();
