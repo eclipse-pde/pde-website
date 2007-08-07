f6d8ce4c69446fd5deff6df201357b9f5f0a6cb7
wmelhem <wmelhem>
2007-08-07 13:11:47 +0000

    added a link

--- pde-ui/committers/committers.php
+++ pde-ui/committers/committers.php
@@ -83,8 +83,8 @@ ob_start();
 			    <td width="33%"><p align="center"><img src="chris_aniszczyk.png" alt="Brock Janiczak" width="200" height="280"></p>
                   <p align="center"><strong>Brock Janiczak</strong></p>
                 </td>
-			    <td width="33%"><p align="center"><img src="bartosz_michalik.PNG" alt="Bartosz Michalik" width="200" height="280"></p>
-                  <p align="center"><strong>Bartosz Michalik</strong></p>
+			    <td width="33%"><p align="center"><img src="bartosz_michalik.PNG" alt="Bartosz Michalik" width="200" height="280" border="0"></p>
+                  <p align="center"><strong><a href="http://eclipser-blog.blogspot.com/">Bartosz Michalik</a></strong></p>
                 </td>
 			   </tr>
           </table>
