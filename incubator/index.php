590126032e486a5491b85c9082d5a404338644f5
mrennie <mrennie>
2007-09-24 14:50:58 +0000

    added API Tooling link

--- incubator/index.php
+++ incubator/index.php
@@ -23,41 +23,38 @@
 ob_start();
 ?>		
 
-	<div id="midcolumn">
-		<h1><?= $pageTitle ?></h1>
-
-			<p class=bar>Mission Statement</p>
-			<p>
-The goal of the PDE Incubator is to do experimental, forward-looking work outside of the constraints of the 
-regular release cycles. While not necessarily targetting the next Eclipse release, our work in the 
-incubator is always focused on delivering new capabilities into the development stream.  
-In particular, the PDE Incubator will focus on tools and technologies to assist with 
-plug-in development, providing plug-in developers more cognitive 
-support for the tasks they perform every day.<BR>
-<BR>
-<I>In PDE we do tooling, but our business is people!</I>
-
-</p>
-
-			<p class=bar>Work Areas</p>
-<table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" >
-	<tr>
-      <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
-      <td width="98%"><a href="dependency-visualization/index.php" target="_top"><b>Dependency Visualization</b></a><br>
-    Views designed to assist with plug-in dependency analysis tasks.</td>
-  </tr>
-  	<tr>
-      <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
-      <td width="98%"><a href="spy/index.php" target="_top"><b>Plug-in Spy</b></a><br>
-    A way for a plug-in developer to introspect Eclipse to aid plug-in development.</td>
-  </tr>
-  </table>
-
-		
-      
-<p>&nbsp;</p>
-	</div>
-
+<div id="midcolumn">
+	<h1><?= $pageTitle ?></h1>
+	<p class=bar>Mission Statement</p>
+		<p>
+		The goal of the PDE Incubator is to do experimental, forward-looking work outside of the constraints of the 
+		regular release cycles. While not necessarily targeting the next Eclipse release, our work in the 
+		incubator is always focused on delivering new capabilities into the development stream.  
+		In particular, the PDE Incubator will focus on tools and technologies to assist with 
+		plug-in development, providing plug-in developers more cognitive 
+		support for the tasks they perform every day.
+		<br>
+		<blockquote><i>In PDE we do tooling, but our business is people!</i></blockquote>
+		</p>
+	<p class=bar>Work Areas</p>
+		<table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" >
+			<tr>
+		      <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+		      <td width="98%"><a href="dependency-visualization/index.php" target="_top"><b>Dependency Visualization</b></a><br>
+			    Views designed to assist with plug-in dependency analysis tasks.</td>
+			  </tr>
+		  	<tr>
+		      <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+		      <td width="98%"><a href="spy/index.php" target="_top"><b>Plug-in Spy</b></a><br>
+		    	A way for a plug-in developer to introspect Eclipse to aid plug-in development.</td>
+		  	</tr>
+		  	<tr>
+		      <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+		      <td width="98%"><a href="http://wiki.eclipse.org/PDE_UI_Incubator_ApiTools" target="_top"><b>API Tooling</b></a><br>
+		    	A collection of utilities that can be used in API maintenance and consistency checks.</td>
+		  	</tr>
+	  	</table>
+</div>
 <?php
 	include $_SERVER['DOCUMENT_ROOT'] . "/pde/incubator/component-links.html";
 	if (file_exists("dir-links.html")) {include "dir-links.html";}
