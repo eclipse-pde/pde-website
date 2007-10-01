2cc159357d393253625b591647fff7e5f7522f3e
bbauman <bbauman>
2007-10-01 19:33:05 +0000

    *** empty log message ***

--- pde-ui/committers/committers.php
+++ pde-ui/committers/committers.php
@@ -83,15 +83,15 @@ ob_start();
 			  <tr>
                 <td width="33%"><p align="center"><img src="peter_friese.JPG" alt="Peter Friese" width="200" height="280"></p>
                   <p align="center"><strong><a href="http://www.peterfriese.de/">Peter Friese</a></strong></p>
-                </td>
+                  <p align="center">&nbsp;</p></td>
 			    <td width="33%"><p align="center"><img src="chris_aniszczyk.png" alt="Brock Janiczak" width="200" height="280"></p>
                   <p align="center"><strong>Brock Janiczak</strong></p>
-                </td>
+                  <p align="center">&nbsp;</p></td>
 			    <td width="33%"><p align="center"><img src="bartosz_michalik.PNG" alt="Bartosz Michalik" width="200" height="280"></p>
                   <p align="center"><strong><a href="http://eclipser-blog.blogspot.com/">Bartosz Michalik</a></strong></p>
-                </td>
-			   </tr>
-			   <tr>
+                  <p align="center">&nbsp;</p></td>
+			  </tr>
+			  <tr>
 			    <td width="33%"><p align="center"><img src="jacek_pospychala.jpg" alt="Jacek Pospychala" width="200" height="280"></p>
 			      <p align="center"><strong>Jacek Pospychala</strong></p>
 			    </td>
