b01ea88fc29a5a6344811af51c772e69a3373588
mrennie <mrennie>
2008-05-01 02:58:17 +0000

    moved documents to webpage

--- pde-api-tools/index.php
+++ pde-api-tools/index.php
@@ -38,23 +38,27 @@ ob_start();
 		<h1><?= $pageTitle ?></h1>
 
 		<div class="homeitem3col">
-		  <h3>About the pde-api-tools component</h3>
-		  	<p>The goal of API Tools is to help the user in maintaining good APIs.
-		  	</p>
+		  <h3>About API Tools</h3>
+		  	<p>The goal of API Tools is to help the user in maintaining good APIs.</p>
 	    </div>
 		<div class="homeitem3col">
 		  <h3>Get involved!</h3>
-		    <p>If you are interested in participating in the development of the PDE
-				API Tools component, check out the developer's mailing list: <a href="http://dev.eclipse.org/mailman/listinfo/pde-ui-build-dev">pde-dev@eclipse.org</a>.
-				Chat with people there about your problems and interests, and find out
-				what you can do to help.
+		    <p>
+		    	If you are interested in participating in the development of the API Tools component, 
+		    	check out the developer's mailing list: <a href="http://dev.eclipse.org/mailman/listinfo/pde-ui-build-dev">pde-dev@eclipse.org</a>.
+				Chat with people there about your problems and interests, and find out what you can do to help.
+			</p>
+			<p>
+				To get started you can check out the source for API Tools using either the <a href="/documents/projectSet.psf">extssh project set file</a> (if you have a bugzilla account) or the 
+				<a href="/documents/pserverProjectSet.psf">pserver project set file</a> (if you do not have a bugzilla account).
 			</p>
 		</div>
 	</div>
 
+	
 	<div id="rightcolumn">
 		<div class="sideitem">
-			<h6>PDE-API Tools links</h6>
+			<h6>API Tools links</h6>
 			<ul>
 				<li><a href="index.php">Home</a></li>
 				<li><a href="http://wiki.eclipse.org/Api_Tooling">WIKI</a></li>
