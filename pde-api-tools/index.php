8b0d042fe9b79ed30d11918a68e2acc278395918
mrennie <mrennie>
2008-08-27 19:41:09 +0000

    *** empty log message ***

--- pde-api-tools/index.php
+++ pde-api-tools/index.php
@@ -1,11 +1,11 @@
-<?php  	
+<?php 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
-	$App 	= new App();	
-	$Nav	= new Nav();	
-	$Menu 	= new Menu();		
-	include($App->getProjectCommon());
+	$App = new App();	
+	$Nav = new Nav();	
+	$Menu = new Menu();		
+	include($App->getProjectCommon());  
 
 	#*****************************************************************************
 	#
@@ -18,32 +18,53 @@
 	#     IBM Corporation - initial implementation
 	#
 	# Author: 		API Tools Team
-	# Date:			August 27, 2008
+	# Date:			April 23, 2008
 	#
-	# Description: 
+	# Description: This is the new main page for the API Tools team website
 	#
 	#
 	#****************************************************************************
 	
-	
-	$pageTitle 		= "";
-	$pageKeywords	= "api, platform, pde, tooling, tools, tags, test, testing, plan, plans, 3.5";
-	$pageAuthor		= "Debug Team";
+	$pageTitle 		= "API Tools";
+	$pageKeywords	= "pde, api, tools";
+	$pageAuthor		= "API Tools Team";
 	
 	include("_sideCommon.php");
 	
-	$html = <<<EOHTML
+	$html = <<<EOHTML	
 
 <div id="maincontent">
 	<div id="midcolumn">
-		<h1>$pageTitle</h1>
+		<h1><?= $pageTitle ?></h1>
+
 		<div class="homeitem3col">
-			<h3>This is a wide column</h3>
-			<ul>
-				<li><a href="#">Link</a>. Teaser text <a href="#">'Reference'</a><span class="dates">02/05/05</span></li>
-				<li><a href="#">Link</a>. Teaser text <a href="#">'Reference'</a><span class="dates">02/05/05</span></li>
-				<li><a href="#">Link</a>. Teaser text <a href="#">'Reference'</a><span class="dates">02/05/05</span></li>
-			</ul>
+		  <h3>Introduction to API Tools</h3>
+		  	<p>  API tooling will assist developers in API maintenance by reporting API defects such as binary incompatibilities, incorrect plug-in version numbers, missing or incorrect @since tags, and usage of non-API code between plug-ins. 
+			  	The tooling will be integrated in the Eclipse SDK and will be used in the automated build process. 
+			  	<br>
+			  	Specifically, the tooling is designed to do the following:
+				<ol>
+				    <li> Identify binary compatibility issues between two versions of a software component or product.</li>
+				    <li> Update version numbers for plug-ins (bundles) based on the Eclipse versioning scheme.</li>
+				    <li> Update @since tags for newly added classes, interfaces, methods, etc.</li>
+				    <li> Provide new javadoc tags and code assist to annotate types with special restrictions.</li>
+				   	<li> Leverage existing information (in MANIFEST.MF) to define the visibility of packages between bundles.</li>
+				    <li> Identify usage of non-API code between plug-ins.</li>
+				    <li> Identity leakage of non-API types into API.</li>
+				</ol>
+			</p>
+	    </div>
+		<div class="homeitem3col">
+		  <h3>Get involved!</h3>
+		    <p>
+		    	If you are interested in participating in the development of the API Tools component, 
+		    	check out the developer's mailing list: <a href="http://dev.eclipse.org/mailman/listinfo/pde-ui-build-dev">pde-dev@eclipse.org</a>.
+				Chat with people there about your problems and interests, and find out what you can do to help.
+			</p>
+			<p>
+				To get started you can check out the source for API Tools using either the <a href="../pde-api-tools/documents/projectSet.psf">extssh project set file</a> (if you have a bugzilla account) or the 
+				<a href="../pde-api-tools/documents/pserverProjectSet.psf">pserver project set file</a> (if you do not have a bugzilla account).
+			</p>
 		</div>
 	</div>
 	<div id="rightcolumn">
@@ -54,5 +75,6 @@
 
 EOHTML;
 
+	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
