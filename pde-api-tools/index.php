1e9a4a74758d055b707d993ba7d4214fde0a3f4e
mrennie <mrennie>
2008-08-27 19:28:58 +0000

    *** empty log message ***

--- pde-api-tools/index.php
+++ pde-api-tools/index.php
@@ -1,13 +1,13 @@
 <?php 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
 
-$App = new App();	
-$Nav = new Nav();	
-$Menu = new Menu();		
+	$App = new App();	
+	$Nav = new Nav();	
+	$Menu = new Menu();		
 
-include($App->getProjectCommon());  
+	include($App->getProjectCommon());  
 
 	#*****************************************************************************
 	#
