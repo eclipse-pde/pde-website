0d21470082d4c65ea6b7a90d54b791367bcd6683
mrennie <mrennie>
2009-01-22 14:59:33 +0000

    updated 3.4.2 test plan

--- pde-api-tools/test_plans/test_plans.php
+++ pde-api-tools/test_plans/test_plans.php
@@ -27,7 +27,7 @@
 	
 	
 	$pageTitle 		= "API Tools Test Plans";
-	$pageKeywords	= "api, platform, pde, tooling, tools, tags, test, testing, plan, plans, 3.5";
+	$pageKeywords	= "api, platform, pde, tooling, tools, tags, test, testing, plan, plans, 3.5, 3.4.2, 3.4.x, 3.4";
 	$pageAuthor		= "API Tools Team";
 	
 	include("../_sideCommon.php");
@@ -45,6 +45,11 @@
 			<ul>
 				<li><a href="/pde/pde-api-tools/test_plans/test_plan-3.5M4.php">3.5M4</a></li>
 				<li><a href="/pde/pde-api-tools/test_plans/test_plan-3.5M2.php">3.5M2</a></li>
+			</ul>
+			<h3>3.4.x Test Plans</h3>
+			<a name="3-4-x_testing"></a>
+			<ul>
+				<li><a href="/pde/pde-api-tools/test_plans/test_plan-3.4.2.php">3.4.2</a></li>
 			</ul>
 		</div>
 	</div>
