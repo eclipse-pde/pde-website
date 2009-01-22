8c53d5099a42a5eb27f9cee5e105db803123998a
mrennie <mrennie>
2009-01-22 20:07:49 +0000

    *** empty log message ***

--- pde-api-tools/_projectCommon.php
+++ pde-api-tools/_projectCommon.php
@@ -34,8 +34,8 @@
 
 	# Defined project nav bar links
 	$Nav->addCustomNav("About API Tools", "/projects/project_summary.php?projectid=pde.apitools", "", 1);
-	$Nav->addCustomNav("API Tools Home", 	"/pde/pde-api-tools/index.php",			"_self", 1);
-	$Nav->addCustomNav("PDE Home", "/pde/", "_self", 1);
+	$Nav->addCustomNav("API Tools Home", 	"/pde/pde-api-tools/index.php",			"", 1);
+	$Nav->addCustomNav("PDE Home", "/pde/", "", 1);
 	
 
 ?>
