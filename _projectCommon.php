46a7580dc856a1abbe1a7f4377a6305ee7df0d72
cwindatt <cwindatt>
2009-02-12 20:33:50 +0000

    Fix project list in left navigation bar

--- _projectCommon.php
+++ _projectCommon.php
@@ -22,11 +22,12 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->addCustomNav("Mailing Lists", 	"http://www.eclipse.org/mail/", "_self", 1);
-	$Nav->addNavSeparator("Components", 	"index.php");
+	$Nav->addNavSeparator("Projects", 	"index.php");
 	$Nav->addCustomNav("Platform", "http://www.eclipse.org/platform/", "_self", 1);
-	$Nav->addCustomNav("Equinox", "http://www.eclipse.org/equinox/", "_self", 1);
 	$Nav->addCustomNav("JDT", "http://www.eclipse.org/jdt/", "_self", 1);
 	$Nav->addCustomNav("PDE", "http://www.eclipse.org/pde/", "_self", 1);
+	$Nav->addCustomNav("e4", "http://www.eclipse.org/e4/", "_self", 1);
+	$Nav->addCustomNav("Incubator", "http://www.eclipse.org/eclipse/incubator/", "_self", 1);
 	$Nav->addNavSeparator("Development", "http://www.eclipse.org/eclipse/development/", "_self", 1);
 
 ?>
--- pde-api-tools/_projectCommon.php
+++ pde-api-tools/_projectCommon.php
@@ -33,6 +33,13 @@
 	}
 
 	# Defined project nav bar links
+	$Nav->addNavSeparator("Projects", 	"index.php");
+	$Nav->addCustomNav("Platform", "http://www.eclipse.org/platform/", "_self", 1);
+	$Nav->addCustomNav("JDT", "http://www.eclipse.org/jdt/", "_self", 1);
+	$Nav->addCustomNav("PDE", "http://www.eclipse.org/pde/", "_self", 1);
+	$Nav->addCustomNav("e4", "http://www.eclipse.org/e4/", "_self", 1);
+	$Nav->addCustomNav("Incubator", "http://www.eclipse.org/eclipse/incubator/", "_self", 1);
+	$Nav->addNavSeparator("Development", "http://www.eclipse.org/eclipse/development/", "_self", 1);
 	$Nav->addCustomNav("About API Tools", "/projects/project_summary.php?projectid=pde.apitools", "", 1);
 	$Nav->addNavSeparator("API Tools Home", "/pde/pde-api-tools/index.php");
 	$Nav->addCustomNav("Milestone Plans", "http://wiki.eclipse.org/ApiTools_Milestones", "", 1);
@@ -40,4 +47,5 @@
 	$Nav->addCustomNav("PDE Build", "/pde/pde-build/", "", 1);
 	$Nav->addCustomNav("PDE Incubator", "http://wiki.eclipse.org/PDE/Incubator", "", 1);
 	$Nav->addCustomNav("PDE UI", "/pde/pde-ui/", "", 1);
+
 ?>
