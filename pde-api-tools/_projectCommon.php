386ad5f432e61c6e518a52ef9a9f28aff2b4b50c
cwindatt <cwindatt>
2009-02-12 21:17:16 +0000

    Fix project list in left navigation bar

--- pde-api-tools/_projectCommon.php
+++ pde-api-tools/_projectCommon.php
@@ -11,7 +11,7 @@
 	#     IBM Corporation - initial implementation
 	#
 	# Author: 		API Tools Team
-	# Date:			January 22, 2009
+	# Date:			February 12, 2009
 	#
 	# Description: This is a contributed item that provides a common left-hand-side 
 	# project side bar for links etc.
@@ -33,7 +33,7 @@
 	}
 
 	# Defined project nav bar links
-	$Nav->addCustomNav("Mailing Lists", 	"http://www.eclipse.org/mail/", "_self", 1);
+	$Nav->addCustomNav("About PDE", "/projects/project_summary.php?projectid=pde", "", 1);
 	$Nav->addNavSeparator("Projects", 	"index.php");
 	$Nav->addCustomNav("Platform", "http://www.eclipse.org/platform/", "_self", 1);
 	$Nav->addCustomNav("JDT", "http://www.eclipse.org/jdt/", "_self", 1);
@@ -41,11 +41,7 @@
 	$Nav->addCustomNav("e4", "http://www.eclipse.org/e4/", "_self", 1);
 	$Nav->addCustomNav("Incubator", "http://www.eclipse.org/eclipse/incubator/", "_self", 1);
 	$Nav->addNavSeparator("Development", "http://www.eclipse.org/eclipse/development/", "_self", 1);
-	$Nav->addNavSeparator("PDE", "/pde/");
-	$Nav->addCustomNav("PDE Build", "/pde/pde-build/", "", 1);
-	$Nav->addCustomNav("PDE UI", "/pde/pde-ui/", "", 1);
-	$Nav->addCustomNav("PDE API Tools", "/pde/pde-api-tools/", "", 1);
-	$Nav->addCustomNav("PDE Incubator", "http://wiki.eclipse.org/PDE/Incubator", "", 1);
+	$Nav->addCustomNav("Mailing Lists", 	"http://www.eclipse.org/mail/", "_self", 1);	
 	$Nav->addNavSeparator("API Tools Home", "/pde/pde-api-tools/index.php");
 	$Nav->addCustomNav("About API Tools", "/projects/project_summary.php?projectid=pde.apitools", "", 1);
 	$Nav->addCustomNav("Milestone Plans", "http://wiki.eclipse.org/ApiTools_Milestones", "", 1);
