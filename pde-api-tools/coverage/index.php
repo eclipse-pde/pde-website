2c9204ce3210653ea2a6157aace38ba0d2b4f4fe
mrennie <mrennie>
2009-03-11 20:08:17 +0000

    updated coverage

--- pde-api-tools/coverage/index.php
+++ pde-api-tools/coverage/index.php
@@ -1,18 +1,31 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php  	
+	#*****************************************************************************
 	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "PDE/API Tools 3.5 Plan";
+	# Copyright (c) 2008, 2009 IBM Corporation and others.
+ 	# All rights reserved. This program and the accompanying materials
+ 	# are made available under the terms of the Eclipse Public License v1.0
+ 	# which accompanies this distribution, and is available at
+ 	# http://www.eclipse.org/legal/epl-v10.html
+ 	# Contributors:
+	#     IBM Corporation - initial implementation
+	#
+	# Author: 		API Tools Team
+	# Date:			
+	#
+	# Description: This is a contributed item that provides a common side bar for links etc.
+	#
+	#****************************************************************************																													
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+$App 	= new App();	
+$Nav	= new Nav();	
+$Menu 	= new Menu();		
+include($App->getProjectCommon());    
+
+	$pageTitle 		= "API Tools Test COverage Results";
 	$pageKeywords	= "PDE, pde, API, Java, plan, planning, JDT, development, tools, ide, Eclipse";
 	$pageAuthor		= "The PDE Team";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
-	# End: page-specific settings
-	#
 
 ob_start();
 ?>
