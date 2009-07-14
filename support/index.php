b40e31b73d08d32c0c8fd930f04963d4d2cf1cbf
caniszczyk <caniszczyk>
2009-07-14 22:19:00 +0000

    adding page for support

---
+++ support/index.php
@@ -0,0 +1,65 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "PDE Support";
+	$pageKeywords	= "pde, support";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>		
+
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+
+		<div class="homeitem3col">
+			<a name="community"></a><h3>Community Newsgroup</h3>
+			<table border="0"> 
+				<tr height="10" colspan="2"></tr>
+        		<tr>
+        		<td width="80" align="center" valign="top" >
+        			&nbsp;&nbsp;&nbsp;&nbsp;<a href="/mylyn/bugs"><img border="0" src="/pde/images/community.jpg"></a>
+        		</td>
+        		 <td>    
+					<p>
+						The PDE Newsgroup is intended for the user community discussions on PDE usage and troubleshooting.
+					</p>
+			    </td>
+				</tr>
+			</table>
+			 <ul>
+				 <li>
+			 		<a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.platform.pde">Browse newsgroup posts</a> using the web interface
+				</li>
+			 	<li>
+					<a href="news://news.eclipse.org/eclipse.platform.pde">Subscribe</a> to the newsgroup with your news reader.
+					<a href="http://wiki.eclipse.org/index.php/Webmaster_FAQ#How_do_I_access_the_Eclipse_newsgroups.3F">See the FAQ</a> for instructions on subscribing
+				</li>
+			 </ul>				    
+		</div>
+		
+		
+	</div>
+
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
