ee3b0f1ec3508c0c8ae6fd01cfa65b3dd8c995d8
cwindatt <cwindatt>
2009-08-31 15:51:00 +0000

    Ankur location

--- pde-ui/committers/committers.php
+++ pde-ui/committers/committers.php
@@ -52,6 +52,7 @@ ob_start();
 			    </td>
 			    <td width="33%"><p align="center"><img src="ankur_sharma.jpg" alt="Ankur Sharma" width="200" height="280"></p>
 			      <p align="center"><strong><a href="http://eclipsepde.wordpress.com/">Ankur Sharma</a></strong></p>
+			      <p align="center">IBM Bangalore</p>
 			      <p align="center">&nbsp;</p></td>
 			  </tr>
 			</td>
